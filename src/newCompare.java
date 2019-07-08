b9e904b5db81afe68f6523f414ff2eba2d56b969
CTC <CTC@LAPTOP-HBRU4R23>
2019-07-08 12:41:47 +0800

    now can pass the fist comparison if line 58 is disable, and lot of code are changed but still not working

--- src/newCompare.java
+++ src/newCompare.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 /*
 	u_exp = currentExp
 	u_act = currentAct
@@ -24,83 +25,77 @@ public class newCompare<T> {
 			for (int i = 0; i < currentExp.children.size(); i++) {
 				NLPTreeNode<T> exp_child = currentExp.children.get(i);
 				NLPTreeNode<T> act_child = currentAct.children.get(i);
-
+				//System.out.println("exp_child = " + exp_child.data + " | act_child = " + act_child.data );//DEBUG
+				
 				match = match && compare_sameStructure(exp_child, act_child);
 			}
 			return match;
-		}catch (IndexOutOfBoundsException err){
+		} catch (IndexOutOfBoundsException err) {
 			System.err.println(err);
 			return false;
 		}
 	}
 
-	public boolean compare_diffStructure(NLPTreeNode<T> currentExp, NLPTreeNode<T> currentAct, NLPTreeNode<T> lastAct){
+	public boolean compare_diffStructure(NLPTreeNode<T> currentExp, NLPTreeNode<T> nextAct, NLPTreeNode<T> lastAct) {
+		// nextAct = the first not used token
+		// lastAct = the last token of the scope
+
 		// BASE case
-		if(currentExp.isLeaf()){
-			if (isIgnore(currentExp.data))
+		System.out.println("currentExp = " + currentExp.data + " | nextAct = " + nextAct.data + " | lastAct = " + lastAct.data);//DEBUG
+		if (currentExp.isLeaf()) {
+			if (isIgnore(currentExp.data)) {
 				return true;
+			}
 
-			while ((currentAct.tokenID <= lastAct.tokenID) && isIgnore(currentAct.data))
-				currentAct = currentAct.getRoot().leafNodeList().get(currentAct.tokenID + 1);
+			while ((nextAct.tokenID <= lastAct.tokenID) && isIgnore(nextAct.data))
+				nextAct = nextAct.getRoot().leafNodeList().get(nextAct.tokenID + 1);
 
-			if (currentAct.tokenID > lastAct.tokenID)
+			if (nextAct.tokenID > lastAct.tokenID) {
+				System.out.println("token bug");//DEBUG
 				return false;
+			}
 
-			currentAct = currentAct.getRoot().leafNodeList().get(currentAct.tokenID + 1);
+			//nextAct = nextAct.getRoot().leafNodeList().get(nextAct.tokenID + 1); //if no this line, it can pass the first comparison
+			
+			System.out.println("Comparing " + currentExp.data + " VS " + nextAct.data + "   ((" + currentExp.data.equals(nextAct.data));
+			System.out.println(currentExp.data + " " + nextAct.data + " " + currentExp.data.equals(nextAct.data));//DEBUG
+			return currentExp.data.equals(nextAct.data);// return compare(currentExp, actSeq[next_act-1])
+		} else { // RECURSIVE Case
+			for (NLPTreeNode<T> exp_child : currentExp.children) {
+				if (!compare_diffStructure(exp_child, nextAct, lastAct))
+					return false;
+			}
 
-			System.out.println("Comparing " + currentExp.data + " VS " + currentAct.data + "   ((" + currentExp.data.equals(currentAct.data));
-			return currentExp.data.equals(currentAct.data);
-		}
-
-		//RECURSIVE Case
-		for (NLPTreeNode<T> exp_child : currentExp.children){
-			if (!compare_diffStructure(exp_child, currentAct, lastAct))
-				return false;
-		}
-
-		if(currentAct.tokenID <= lastAct.tokenID){
-			/*
-			if there are remaining tokens that are not ignored
-				return false
-			 */
-			for (NLPTreeNode<T> token: currentAct.getRoot().leafNodeList()) {
-				if (token.tokenID > currentAct.tokenID) {
-					if (!isIgnore(token.data)) {
-						return false;
+			if (nextAct.tokenID <= lastAct.tokenID) {
+				/*
+				 * if there are remaining tokens that are not ignored return false
+				 */
+				for (NLPTreeNode<T> token : nextAct.getRoot().leafNodeList()) {
+					if (token.tokenID > nextAct.tokenID) {
+						if (!isIgnore(token.data)) {
+							return false;
+						}
 					}
 				}
 			}
+			return true;
 		}
-
-		return true;
 	}
 
-	public boolean isIgnore(T word){
+	public boolean isIgnore(T word) {
 		if (stop_word.contains(word))
 			return true;
 		return false;
 	}
-	
-	public String getType(String data) {
-		String type = "";
-		try {
-			Double.parseDouble(data);
-			if (data.contains(".")) {
-				type = "DOUBLE";
-			} else {
-				try {
-					Integer.parseInt(data); // because value "1F" is considered as double which has no ".", treat is as
-											// string
-					type = "INT";
-				} catch (NumberFormatException e) {
-					type = "STR";
-				}
-			}
-		} catch (NumberFormatException e) {
-			type = "STR";
-		}
-		return type;
-	}
+
+	/*
+	 * public String getType(String data) { String type = ""; try {
+	 * Double.parseDouble(data); if (data.contains(".")) { type = "DOUBLE"; } else {
+	 * try { Integer.parseInt(data); // because value "1F" is considered as double
+	 * which has no ".", treat is as // string type = "INT"; } catch
+	 * (NumberFormatException e) { type = "STR"; } } } catch (NumberFormatException
+	 * e) { type = "STR"; } return type; }
+	 */
 
 	public static void main(String[] args) {
 		coreNLPOutput NLPTree = new coreNLPOutput();
@@ -110,9 +105,12 @@ public class newCompare<T> {
 
 		System.out.println(compare.compare_sameStructure(test_exp.get(0), test_act.get(0)));
 		System.out.println("-----------------------------------------------------");
-		System.out.println(compare.compare_diffStructure(test_exp.get(0), test_act.get(0), test_act.get(test_act.size()-1)));
+		
+		NLPTreeNode<String> a = test_act.get(0).getRoot().leafNodeList().get(0);
+		NLPTreeNode<String> b = test_act.get(0).getRoot().leafNodeList().get(test_act.get(0).getRoot().leafNodeList().size()-1);
+		
+		System.out.println(
+				compare.compare_diffStructure(test_exp.get(0), a, b));
 	}
 
-	
-	
 }
